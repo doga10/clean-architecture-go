e76c05430c8d9c62aa35594691fef18d36c17b9a
Douglas Dennys <douglasdennys@yahoo.com>
2020-07-12 17:13:17 -0300

    iniciando testes do modulo de loadById

--- src/presentation/controllers/account/add-account/add-account-controller.go
+++ src/presentation/controllers/account/add-account/add-account-controller.go
@@ -2,7 +2,6 @@ package add_account
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/doga10/clean-architecture-go/src/domain/usecases/account"
 	"github.com/doga10/clean-architecture-go/src/presentation/protocols"
 )
